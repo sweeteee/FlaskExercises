f1a91783db9923db3f6b8eca9fda551bfcabc9ef
sweeteee <beanbean126@126.com>
2025-01-05 10:36:23 +0800

    对flaskProject2中的datetime_format函数做了注释

--- flaskProject2/app.py
+++ flaskProject2/app.py
@@ -1,8 +1,12 @@
+# render_template: 这是 Flask 提供的一个函数，用来渲染模板（如 HTML 文件），并将数据传递到模板中。
 from flask import Flask,render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
+# value 是传递给该函数的参数，通常是一个 datetime 对象。
+# format 是一个可选的字符串参数，指定日期的格式。默认格式是 "%Y-%m-%d %H:%M"，表示年份-月份-日期 小时:分钟的格式（例如 2025-01-05 15:30）。
+# value.strftime(format) 会将 datetime 对象格式化为指定格式的字符串。
 def datetime_format(value,format="%Y-%m-%d %H:%M"):
     return value.strftime(format)
 
